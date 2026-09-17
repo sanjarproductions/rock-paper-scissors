4927f07bc9a81543003851826ca0fb7cb55d42ee
Sanjar Kamaliddinov <sanjarkama26@gmail.com>
2026-09-17 11:34:29 +0500

    fix: remove broken index.css import in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import App from "./App.jsx";
 import store from "./redux/store.js";
 
-import "./index.css";
+// import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
